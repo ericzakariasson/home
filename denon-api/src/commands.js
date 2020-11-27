286baff7468d00caeee543df0043633d3e6d97b4
ericzakariasson <eric.zakariasson@gmail.com>
2020-11-27 20:46:55 +0100

    feat: add set power command

--- denon-api/src/commands.js
+++ denon-api/src/commands.js
@@ -1,12 +1,14 @@
 const commands = {
   SET_VOLUME: "SET_VOLUME",
   SET_INPUT: "SET_INPUT",
+  SET_POWER: "SET_POWER",
 };
 
 const commandHandlers = {
   [commands.SET_VOLUME]: (value, client) =>
     client.setVolume(Math.min(parseInt(value), 50)),
   [commands.SET_INPUT]: (value, client) => client.setInput(value),
+  [commands.SET_POWER]: (value, client) => client.setPower(value),
 };
 
 const commandHandlerMap = new Map(Object.entries(commandHandlers));
--- denon-api/src/validation.js
+++ denon-api/src/validation.js
@@ -5,6 +5,8 @@ const validation = {
   [commands.SET_VOLUME]: (value) => 0 <= value && value <= 100,
   [commands.SET_INPUT]: (value) =>
     Object.values(Options.InputOptions).includes(value),
+  [commands.SET_POWER]: (value) =>
+    Object.keys(Options.PowerOptions).includes(value),
 };
 
 const validationMap = new Map(Object.entries(validation));
